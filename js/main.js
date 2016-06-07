3ea008e6c0f21031d97bff881516ad4623dac2e8
Ferruzo <kiara_ferruzo@hotmail.es>
2016-06-07 14:52:14 -0500

    hasta aquí llegué

--- js/main.js
+++ js/main.js
@@ -2,9 +2,15 @@ $(document).ready(function(){
     $('#mainInput').keypress(function(e){
         if(e.which == 13){
             var value = $('#mainInput').val()
-            var time = new Date()
-            $('.w-message-list').append('<div class="w-message w-message-out "><div class="w-message-text"><p>' + value + '</p><div>' + time + '</div></div></div>')
+            var time = new Date(),
+            hours = time.getHours(),
+            minutes = time.getMinutes()
+            $('.w-message-list').append('<div class="w-message w-message-out "><div class="w-message-text"><p>' + value + '</p><div class="time">' + hours + ':' + minutes + '</div></div></div>')
             $('#mainInput').val('')
         }
     })
+})
+
+$('.avatar').click(function(){
+    $('.w-message-list').empty()
 })
